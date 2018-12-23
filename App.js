2acda587e96e114c6c06fa625d07abb2d527ff3a
mnindrazaka <mnindrazaka@gmail.com>
2018-12-23 16:01:15 +0700

    add tsx and ant-design

--- App.js
+++ App.js
@@ -1,18 +1,2 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Examples } from '@shoutem/ui'
-
-export default class App extends React.Component {
-  render() {
-    return <Examples />
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+import App from './build/App'
+export default App
