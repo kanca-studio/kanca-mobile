cc17339a69cdaf59bcbb9d7039a4307504610182
Made Baruna <made.setia@gmail.com>
2018-12-24 16:14:17 +0700

    Update login page

--- src/components/InputItem.tsx
+++ src/components/InputItem.tsx
@@ -1,10 +1,16 @@
-import React, { Component } from "react"
-import { TextInputProps, StyleSheet, View, Text, TextInput } from "react-native"
-import constants from "../config/constants"
+import React, { Component, ReactElement, cloneElement } from "react"
+import {
+  TextInputProps,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+} from "react-native"
 
 interface IProps extends TextInputProps {
-  label: string
+  label?: string
   placeholder: string
+  icon?: ReactElement<any>
 }
 
 interface IState {
@@ -12,25 +18,36 @@ interface IState {
 }
 
 export default class InputItem extends Component<IProps, IState> {
-  state = {
-    isFocused: false
+  public state = {
+    isFocused: false,
   }
 
-  render() {
+  public render() {
     const props = this.props
+    const Icon = props.icon
     return (
       <View style={styles.container}>
-        <View style={styles.labelContainer}>
-          <Text>{props.label}</Text>
-        </View>
+        {!!!props.icon && (
+          <View style={styles.labelContainer}>
+            <Text>{props.label}</Text>
+          </View>
+        )}
         <View
           style={[
             styles.inputContainer,
-            { borderColor: this.state.isFocused ? "rgb(31,144,230)" : "grey" }
+            { borderColor: this.state.isFocused ? "rgb(31,144,230)" : "grey" },
           ]}
         >
+          {!!props.icon && (
+            <View style={styles.iconContainer}>
+              {cloneElement(Icon, {
+                color: this.state.isFocused ? "rgb(31,144,230)" : "grey",
+              })}
+            </View>
+          )}
           <TextInput
             {...props}
+            style={styles.input}
             underlineColorAndroid={"transparent"}
             placeholder={props.placeholder}
             onFocus={() => this.setState({ isFocused: true })}
@@ -44,28 +61,34 @@ export default class InputItem extends Component<IProps, IState> {
 
 const styles = StyleSheet.create({
   container: {
-    width: constants.DEVICE_WIDTH * 0.95,
     height: 50,
-    padding: 10,
     flexDirection: "row",
-    marginVertical: 10,
+    marginBottom: 10,
     alignSelf: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
-
+  iconContainer: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingRight: 10,
+  },
   labelContainer: {
     flex: 1,
     height: 50,
     justifyContent: "center",
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
-
   inputContainer: {
+    flexDirection: "row",
     borderRadius: 5,
     borderWidth: 1,
-    flex: 4,
+    flex: 1,
     paddingHorizontal: 10,
     height: 50,
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+  },
+  input: {
+    flex: 1,
+  },
 })
--- src/pages/auth/Login/index.tsx
+++ src/pages/auth/Login/index.tsx
@@ -1,7 +1,12 @@
 import { Button } from "@ant-design/react-native"
 import React, { Component } from "react"
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet, Image } from "react-native"
 import { NavigationScreenProp } from "react-navigation"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+
+import Logo from "../../../../assets/logo.png"
+import constant from "../../../config/constants"
+import InputItem from "../../../components/InputItem"
 
 interface IProps {
   navigation: NavigationScreenProp<any, any>
@@ -10,14 +15,44 @@ interface IProps {
 export default class Login extends Component<IProps> {
   public render() {
     return (
-      <View>
-        <Text>Login</Text>
-        <Button
-          type="primary"
-          onPress={() => this.props.navigation.navigate("CommunityList")}>
-          Go To Community List
-        </Button>
+      <View style={styles.wrapper}>
+        <Image source={Logo} resizeMode="contain" style={styles.logo} />
+        <View style={styles.form}>
+          <InputItem
+            icon={
+              <MaterialCommunityIcons name="email" size={32} />
+            }
+            placeholder="Your email address"
+          />
+          <InputItem
+            secureTextEntry
+            icon={<MaterialCommunityIcons name="lock" size={32} />}
+            placeholder="Your password"
+          />
+          <Button type="primary">Login</Button>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    justifyContent: "center",
+    flex: 1,
+  },
+  logo: {
+    alignSelf: "center",
+    width: constant.DEVICE_WIDTH - 40,
+    height: 200 * ((constant.DEVICE_WIDTH - 40) / 499),
+  },
+  heading: {
+    alignSelf: "center",
+    fontSize: 24,
+    marginVertical: 10,
+  },
+  form: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+})
